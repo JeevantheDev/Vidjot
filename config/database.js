744eefd28018d65b0b507e254985097cd4949515
Jeevan Jyoti Dash <dashjeevanjyoti04@gmail.com>
2019-07-08 10:38:52 +0530

    added files

--- config/database.js
+++ config/database.js
@@ -1,5 +1,5 @@
 if(process.env.NODE_ENV === 'production'){
-    module.exports = {mongoURI: ''}
+    module.exports = {mongoURI: 'mongodb://jeevanjyotidash:myfamily4@myfirstcluster-0cfh2.mongodb.net/test?retryWrites=true&w=majority'}
   } else {
     module.exports = {mongoURI: 'mongodb://localhost/vidjot-dev'}
   }
