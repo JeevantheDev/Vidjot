ef4a03d3ec881d379b22773c0c815286e1edb1c4
Jeevan Jyoti Dash <dashjeevanjyoti04@gmail.com>
2019-07-08 13:00:42 +0530

    make it better

--- config/database.js
+++ config/database.js
@@ -1,5 +1,5 @@
 if(process.env.NODE_ENV === 'production'){
-    module.exports = {mongoURI: 'mongodb+srv://jjdash:jeevan04@cluster0-0pbd0.mongodb.net/test?retryWrites=true&w=majority'}
+    module.exports = {mongoURI: 'mongodb://jjdash:jeevan04@cluster0-0pbd0.mongodb.net/test?retryWrites=true&w=majority'}
   } else {
     module.exports = {mongoURI: 'mongodb://localhost/vidjot-dev'}
   }
