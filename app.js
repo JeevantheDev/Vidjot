c20feddc38342e9d4e42dd7ef35b707faaf045b1
Jeevan Jyoti Dash <dashjeevanjyoti04@gmail.com>
2019-07-08 10:31:11 +0530

    added files

--- app.js
+++ app.js
@@ -16,11 +16,13 @@ const users = require('./routes/users');
 
 // Passport Config
 require('./config/passport')(passport);
+//DB config
+const db = require('./config/database');
 
 // Map global promise - get rid of warning
 mongoose.Promise = global.Promise;
 // Connect to mongoose
-mongoose.connect('mongodb://localhost/vidjot-dev', {
+mongoose.connect(db.mongoURI, {
   useMongoClient: true
 })
   .then(() => console.log('MongoDB Connected...'))
@@ -82,7 +84,7 @@ app.get('/about', (req, res) => {
 app.use('/ideas', ideas);
 app.use('/users', users);
 
-const port = 5000;
+const port =process.env.PORT || 5000;
 
 app.listen(port, () =>{
   console.log(`Server started on port ${port}`);
