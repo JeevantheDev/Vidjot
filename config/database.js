2774d1a9ea40eafcbc143c31fc520840852daf5f
Jeevan Jyoti Dash <dashjeevanjyoti04@gmail.com>
2020-07-02 09:30:52 +0530

    final commit

--- config/database.js
+++ config/database.js
@@ -1,5 +1,5 @@
 if(process.env.NODE_ENV === 'production'){
     module.exports = {mongoURI: 'mongodb://jjdash:jeevan04@cluster0-0pbd0.mongodb.net/test?retryWrites=true&w=majority'}
   } else {
-    module.exports = {mongoURI: 'mongodb://localhost/vidjot-dev'}
+    module.exports = {mongoURI: 'mongodb://localhost:27017/vidjot-dev'}
   }
